299670095639ef51e993f6e7b6d2535f22e3fa72
Maldewar <2597672+mldwr@users.noreply.github.com>
2024-01-30 16:27:07 +0100

    fix navigation menu from md to lg

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+    <div className="flex h-screen flex-col lg:flex-row lg:overflow-hidden">
+      <div className="w-full flex-none lg:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 lg:overflow-y-auto lg:p-12">{children}</div>
     </div>
   );
 }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -33,14 +33,14 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-green-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-green-600 lg:flex-none lg:justify-start lg:p-2 lg:px-3',
               {
                 'bg-sky-100 text-green-600': pathname === link.href,
               },
             )}
           >
             <LinkIcon className="w-6"/>
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden lg:block">{link.name}</p>
           </Link>
         );
       })}
