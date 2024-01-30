e637b53bb78467eb2e6c9ed4b575c5c9bd76e9b3
Maldewar <2597672+mldwr@users.noreply.github.com>
2024-01-30 20:28:38 +0100

    fixed overlapping table elements

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -60,7 +60,7 @@ export default async function InvoicesTable({
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                <th scope="col" className="px-12 py-5 font-medium sm:pl-6">
                   Übungsleiter
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
